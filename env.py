46355dc9d60aecd5c20aa3cc3f8cfcd63659575a
pauldardis <paul.dardis@bt.com>
2020-04-12 09:51:51 +0000

    adding search app and strip and env.py file

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE","pk_test_l6lZZNe7hQH5Vw0q1V0uxfDu00Bk3fXSRy")
+os.environ.setdefault("STRIPE_SECRET","sk_test_uglR4jHDRZV4jZzrGWiH4u9O00loyCmNcz")
